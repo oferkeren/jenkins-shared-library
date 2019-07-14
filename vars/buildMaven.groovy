b3b8e94c468f8c3c977d04c539642c25099e35f3
oferkeren <oferkeren@gmail.com>
2019-07-14 11:24:15 +0300

    Update buildMaven.groovy

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -1,3 +1,3 @@
-def mvn(servicePath, dragLine) {
-    sh 'cd ${servicePath} && mvn -DargLine="${dragLine}"'
+def mvn() {
+    sh 'cd /home/jenkins/CCMService && mvn -DargLine="-Xms2g -Dconfig.resource=local.conf -Dorg.slf4j.simpleLogger.log.scala.slick=warn" clean package'
 }
